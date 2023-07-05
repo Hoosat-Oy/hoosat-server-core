894468ffe31a475a9a3f8281b9032fa31979d1c7
Toni Lukkaroinen <tmkcodes@gmx.com>
2023-07-05 13:21:02 +0300

    Update upload.ts to include the new filename and path.

--- upload.ts
+++ upload.ts
@@ -2,10 +2,13 @@ import { createReadStream, createWriteStream } from "fs";
 import { v4 as uuidv4 } from "uuid";
 import { HoosatRequest, HoosatRequestHandler, HoosatResponse } from "./types";
 
+
 export const upload = (_uploadLocation: string, _maxFileSize: number = 10 * 1024 * 1024): HoosatRequestHandler => {
   return async (req: HoosatRequest, res: HoosatResponse, next?: HoosatRequestHandler) => {
 
     if (req.headers['content-type'] && req.headers['content-type'].startsWith('multipart/form-data')) {
+
+      // Create an array to store the updated file information
 
       // Iterate over the keys in the request body
       for (const key in req.files) {
@@ -25,9 +28,10 @@ export const upload = (_uploadLocation: string, _maxFileSize: number = 10 * 1024
               // Pipe the file stream (writeStream) to the write stream to save the file
               const readStream = createReadStream(file.filepath);
               readStream.pipe(writeStream);
-
+              file.filepath = filePath;
+              file.newFilename = `${uuidv4()}-${file.originalFilename}`;
               // Log the file path
-              console.log(`File saved: ${filePath}`);
+              console.log(`File saved: ${file.filepath}`);
             }
           }
         } else {
