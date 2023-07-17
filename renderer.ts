5514fee6729cc53cd39f9a07a637e13d2fac64aa
Toni Lukkaroinen <tmkcodes@gmx.com>
2023-07-17 10:09:53 +0300

    Changes to renderer.

--- renderer.ts
+++ renderer.ts
@@ -3,17 +3,33 @@ import { FilledContext } from "react-helmet-async";
 import { extractCssFrom } from "./cssExtractor";
 import { generatePreloadTags } from "./preload";
 import { replaceHeadTags } from "./seo";
-import { HoosatResponse } from "./types";
-import { DEBUG, ErrorHandler } from "./errors";
+import { HeadTags, HoosatResponse } from "./types";
+import { ErrorHandler } from "./errors";
 import { ReactNode, JSX } from "react";
+import internal from "stream";
 
 
 interface HoosatRenderer {
   res: HoosatResponse,
   jsx: ReactNode | JSX.Element,
-  helmetContext: object,
-  extractCSS: boolean,
-  preloadTagFolder: string,
+  helmetContext?: object,
+  extractCSS?: boolean,
+  preloadTagFolder?: string,
+  headTags?: HeadTags,
+}
+
+
+export const helmetStream = async (headTags: HeadTags, helmetContext: object): Promise<internal.Transform> => {
+  const { helmet } = helmetContext as FilledContext;
+  const replaceStream = replaceHeadTags({
+    title: headTags.title + helmet?.title?.toString(),
+    meta: headTags.meta + helmet?.meta?.toString(),
+    link: headTags.link + (helmet?.link?.toString()),
+    script: headTags.script + helmet?.script?.toString(),
+    base: headTags.base + helmet?.base?.toString(),
+    style: headTags.style + helmet?.style?.toString(),
+  });
+  return replaceStream;
 }
 
 /**
@@ -24,39 +40,35 @@ interface HoosatRenderer {
  * @returns {void}
  */
 export const renderer = (params: HoosatRenderer): void => {
-  const { res, jsx, helmetContext, extractCSS, preloadTagFolder } = params;
+  const { res, jsx, helmetContext, extractCSS, preloadTagFolder, headTags } = params;
   let css = "";
   if(extractCSS == true) {
     css = extractCssFrom("./src/client");
   }
   let preloadTags: string[];
   if(process.env.NODE_ENV === "development") {
-    preloadTags = generatePreloadTags(preloadTagFolder, "/");
+    preloadTags = generatePreloadTags(preloadTagFolder!, "/");
   } else {
     preloadTags = generatePreloadTags(process.env.PRELOAD_TAG_FOLDER!, "/");
   }
-  // Rendering the App component to a pipeable stream
   const stream = renderToPipeableStream(
     jsx,
     {
       bootstrapModules: ["/bundle.js"],
       onShellReady: async () => {
-        const { helmet } = helmetContext as FilledContext;
-        while (!helmet?.title) {
-          await new Promise(resolve => setTimeout(resolve, 50));
-        }
-        DEBUG.log(helmet);
-        DEBUG.log(helmet.title.toString());
-        const replaceStream = replaceHeadTags({
-          title: helmet?.title?.toString(),
-          meta: helmet?.meta?.toString(),
-          link: preloadTags.join("\n") + (helmet?.link?.toString()),
-          script: helmet?.script?.toString(),
-          base: helmet?.base?.toString(),
-          style: "<style>" + css + "</style>" + helmet?.style?.toString(),
-        });
         res.setHeader('content-type', 'text/html');
-        stream.pipe(replaceStream).pipe(res);
+        if (helmetContext !== undefined) {
+          let newTags: HeadTags = {};
+          if (headTags !== undefined) {
+            newTags = headTags;
+          }
+          newTags.link = newTags.link + preloadTags.join("\n");
+          newTags.style = newTags.style + "<style>" + css + "</style>";
+          const replaceStream = await helmetStream(newTags, helmetContext);
+          stream.pipe(replaceStream).pipe(res);
+        } else {
+          stream.pipe(res);
+        }
       },
       onShellError(error) {
         console.error(error);
