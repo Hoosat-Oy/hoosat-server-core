c354e24f2548b1aedf1a033e4b07ac35d7a908a1
Toni Lukkaroinen <toni.lukkaroinen@gmail.com>
2024-01-16 10:09:00 +0200

    Fix compressed asset loading.

--- assets.ts
+++ assets.ts
@@ -8,7 +8,7 @@ import { HoosatRequest, HoosatRequestHandler, HoosatResponse } from './types';
  * @param {string} publicPath - The path to the public directory where the assets are located.
  * @returns {HoosatRequestHandler} The CORS middleware.
  */
-export const assets = (publicPath: string, enableCompression = false): HoosatRequestHandler => {
+export const assets = (publicPath: string): HoosatRequestHandler => {
   /**
    * A request handler function.
    *
@@ -25,15 +25,10 @@ export const assets = (publicPath: string, enableCompression = false): HoosatReq
         decodedURL = decodedURL.slice('/public'.length);
       }
     } catch (error) {
-      // Handle the malformed URI error here
       console.error('Malformed URI:', error);
-      decodedURL = '';
+      return;
     }
-
-    let acceptEncoding = req.headers['Accept-Encoding'] as string;
-    if (enableCompression === false) {
-      acceptEncoding = '';
-    }
+    let acceptEncoding = req.headers['accept-encoding'] as string;
 
     const filePath = path.join(publicPath, decodedURL);
     // Check if filePath is a file
@@ -41,112 +36,80 @@ export const assets = (publicPath: string, enableCompression = false): HoosatReq
       return next && next(req, res);
     }
 
-    const getContenType = (ext: string) => {
-      if (ext === '.jpg' || ext === '.jpeg') {
-        contentType = 'image/jpeg';
-      } else if (ext === '.png') {
-        contentType = 'image/png';
-      } else if (ext === '.gif') {
-        contentType = 'image/gif';
-      } else if (ext === '.avif') {
-        contentType = 'image/avif';
-      } else if (ext === '.svg') {
-        contentType = 'image/svg+xml';
-      } else if (ext === '.otf') {
-        contentType = 'font/otf';
-      } else if (ext === '.otf') {
-        contentType = 'font/otf';
-      } else if (ext === '.ttf') {
-        contentType = 'font/ttf';
-      } else if (ext === '.pdf') {
-        contentType = 'application/pdf';
-      } else if (ext === '.txt') {
-        contentType = 'text/plain';
-      } else if (ext === '.csv') {
-        contentType = 'text/csv';
-      } else if (ext === '.json') {
-        contentType = 'application/json';
-      } else if (ext === '.xml') {
-        contentType = 'application/xml';
-      } else if (ext === '.js') {
-        contentType = 'application/javascript';
-      } else if (ext === '.css') {
-        contentType = 'text/css';
-      } else {
-        contentType = 'application/octet-stream';
-      }
-      return contentType
-    }
-
-    const getCacheControl = (ext: string) => {
-      if (ext === '.jpg' || ext === '.jpeg') {
-        cacheControl = 'public, max-age=31536000';
-      } else if (ext === '.png') {
-        cacheControl = 'public, max-age=31536000';
-      } else if (ext === '.gif') {
-        cacheControl = 'public, max-age=31536000';
-      } else if (ext === '.avif') {
-        cacheControl = 'public, max-age=31536000';
-      } else if (ext === '.svg') {
-        cacheControl = 'public, max-age=31536000';
-      } else if (ext === '.otf') {
-        cacheControl = 'public, max-age=31536000';
-      } else if (ext === '.otf') {
-        cacheControl = 'public, max-age=31536000';
-      } else if (ext === '.ttf') {
-        cacheControl = 'public, max-age=31536000';
-      } else if (ext === '.pdf') {
-        cacheControl = 'must-revalidate, public, max-age=31536000';
-      } else if (ext === '.txt') {
-        cacheControl = 'must-revalidate, public, max-age=14400';
-      } else if (ext === '.csv') {
-        cacheControl = 'must-revalidate, public, max-age=14400';
-      } else if (ext === '.json') {
-        cacheControl = 'must-revalidate, public, max-age=14400';
-      } else if (ext === '.xml') {
-        cacheControl = 'must-revalidate, public, max-age=14400';
-      } else if (ext === '.js') {
-        cacheControl = 'must-revalidate, max-age=14400';
-      } else if (ext === '.css') {
-        cacheControl = 'must-revalidate, max-age=14400';
-      } else {
-        cacheControl = 'no-cache'
-      }
-      return cacheControl
-    }
-    
     const ext = path.extname(filePath).toLowerCase();
-    let contentType = 'application/octet-stream';
-    let cacheControl = 'no-cache';
 
-    // Check for compressed files in order and send the compressed file. 
-    const compressedExtensions = [".br", ".deflate", ".gzip"];
-    for (const encoding of compressedExtensions) {
-      if (acceptEncoding.includes(encoding)) {
-        const compressedFilePath = filePath + encoding;
-        const contentType = getContenType(ext);
-        const cacheControl = getCacheControl(ext);
-        if(fs.existsSync(compressedFilePath)) {
-          const compressedFileStream = fs.createReadStream(compressedFilePath);
-          console.log(compressedFilePath);
-          console.log(ext);
-          console.log(encoding);
-          console.log(contentType);
-          console.log(cacheControl);
-          res.setHeader("Content-Encoding", encoding);
-          res.setHeader('Content-Type', contentType);
-          res.setHeader('Cache-Control', cacheControl);
-          compressedFileStream.pipe(res);
-          return;
+    const contentTypeMap: { [key: string]: string } = {
+      '.jpg': 'image/jpeg',
+      '.jpeg': 'image/jpeg',
+      '.png': 'image/png',
+      '.gif': 'image/gif',
+      '.avif': 'image/avif',
+      '.svg': 'image/svg+xml',
+      '.otf': 'font/otf',
+      '.ttf': 'font/ttf',
+      '.pdf': 'application/pdf',
+      '.txt': 'text/plain',
+      '.csv': 'text/csv',
+      '.json': 'application/json',
+      '.xml': 'text/xml',
+      '.html': 'text/html',
+      '.xhtml': 'application/xhtml+xml',
+      '.js': 'application/javascript',
+      '.css': 'text/css',
+      '.mp4': 'video/mp4',
+      '.mp3': 'audio/mp3',
+      '.wav': 'audio/wav',
+      '.ico': 'image/x-icon',
+    };
+
+    const cacheControlMap: { [key: string]: string } = {
+      '.jpg': 'public, max-age=31536000',
+      '.jpeg': 'public, max-age=31536000',
+      '.png': 'public, max-age=31536000',
+      '.gif': 'public, max-age=31536000',
+      '.avif': 'public, max-age=31536000',
+      '.svg': 'public, max-age=31536000',
+      '.otf': 'public, max-age=31536000',
+      '.ttf': 'public, max-age=31536000',
+      '.pdf': 'must-revalidate, public, max-age=31536000',
+      '.txt': 'must-revalidate, public, max-age=14400',
+      '.csv': 'must-revalidate, public, max-age=14400',
+      '.json': 'must-revalidate, public, max-age=14400',
+      '.xml': 'must-revalidate, public, max-age=14400',
+      '.html': 'must-revalidate, public, max-age=14400',
+      '.xhtml': 'must-revalidate, public, max-age=14400',
+      '.js': 'must-revalidate, max-age=14400',
+      '.css': 'must-revalidate, public, max-age=14400',
+      '.mp4': 'public, max-age=31536000',
+      '.mp3': 'public, max-age=31536000',
+      '.wav': 'public, max-age=31536000',
+      '.ico': 'public, max-age=31536000',
+    };
+    
+    const contentType = contentTypeMap[ext] || 'application/octet-stream';
+    const cacheControl = cacheControlMap[ext] || 'no-cache';
+
+    if (acceptEncoding !== undefined) {
+      const compressedExtensions = ['.br', '.deflate', '.gzip'];
+      for (const encoding of compressedExtensions) {
+        if (acceptEncoding.includes(encoding.replace(".", ""))) {
+          const compressedFilePath = filePath + encoding;
+          if (fs.existsSync(compressedFilePath)) {
+            const compressedFileStream = fs.createReadStream(compressedFilePath);
+            res.setHeader('Content-Encoding', encoding.replace(".", ""));
+            res.setHeader('Content-Type', contentType);
+            res.setHeader('Cache-Control', cacheControl);
+            compressedFileStream.pipe(res);
+            return;
+          }
         }
       }
     }
-    contentType = getContenType(ext);
-    cacheControl = getCacheControl(ext);
+
+    // If no compressed file found, serve the uncompressed file
     res.setHeader('Content-Type', contentType);
     res.setHeader('Cache-Control', cacheControl);
     const fileStream = fs.createReadStream(filePath);
     fileStream.pipe(res);
-    // If no compressed file found, send the uncompressed file
   };
 };
